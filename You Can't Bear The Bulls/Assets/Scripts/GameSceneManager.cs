044c9256fb1430b89cbc3633ca7710ecb6b96d87
intelatrix <mrallson@hotmail.com>
2015-12-11 20:46:33 +0800

    Retry

---
+++ You Can't Bear The Bulls/Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,173 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameSceneManager : MonoSingleton<GameSceneManager>
+{ 
+    List<BasicBull> ListOfBullLeft = new List<BasicBull>();
+    List<BasicBull> ListOfBullRight = new List<BasicBull>();
+    List<BasicBull> ListofAllBulls = new List<BasicBull>();
+
+    bool IfMiss = false;
+    public float ConstMissTime = 1.0f;
+  
+    float MissTimeLeft = 0f;
+
+    enum GameState
+    {
+    	GAME_NORMAL,
+    	GAME_MOTHER_QTE,
+    	GAME_FATHER_QTE
+    };
+
+    GameState CurrentState = GameState.GAME_NORMAL;
+
+    void Start()
+    {
+  
+    }
+
+    void Update()
+    {
+        GameUpdate();
+    }
+    
+    void GameUpdate()
+    {
+    	switch(CurrentState)
+    	{
+    	case GameState.GAME_NORMAL:
+        	NormalGameUpdate();
+        	break;
+        case GameState.GAME_MOTHER_QTE:
+        	break;
+        case GameState.GAME_FATHER_QTE:
+       		break;
+        }
+    }
+
+    void NormalGameUpdate()
+    {
+	    if(!IfMiss)
+	    {
+	        ArrowKeysPressed CurrentKeyPressed = NormalControlsUpdate();
+	        List<BasicBull> TempList = null;
+	        bool? IfPressRight = null;
+	        switch (CurrentKeyPressed)
+	        {
+	            case ArrowKeysPressed.KEYS_LEFT:
+	                TempList = ListOfBullLeft;
+	                IfPressRight = false;
+	                break;
+	            case ArrowKeysPressed.KEYS_RIGHT:
+	                TempList = ListOfBullRight;
+	                IfPressRight = true;
+	                break;
+	        }
+
+	        if (TempList == null)
+	        {
+	           
+	        }
+	        else 
+	        {
+				if (TempList.Count > 0)
+	       		{
+					BearPunchBull(TempList);
+		        }
+		        else
+		        {
+	        		Player_Bear.Instance.SetBearMiss((bool)IfPressRight);
+					MissTimeLeft =  ConstMissTime;
+					IfMiss = true;
+	       		}
+	        }
+        }
+        else
+        {
+			MissTimeLeft -= TimeManager.Instance.GetGameDeltaTime();
+			if(MissTimeLeft <=0)
+				IfMiss = false;
+
+			//Player_Bear.Instance.StopMissing();
+        }
+    }
+
+    void MotherGameUpdate()
+    {
+	
+    }
+
+    ArrowKeysPressed MotherControlUpdate()
+    {
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+            return ArrowKeysPressed.KEYS_LEFT;
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            return ArrowKeysPressed.KEYS_RIGHT;
+
+		else if (Input.GetKeyDown(KeyCode.UpArrow))
+            return ArrowKeysPressed.KEYS_UP;
+
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+            return ArrowKeysPressed.KEYS_DOWN;
+
+        else
+            return ArrowKeysPressed.KEYS_NONE;
+    }
+
+    void BearPunchBull(List<BasicBull> TempList)
+    {
+		BasicBull TempBull = TempList[0];
+
+		switch(TempBull.BullType)
+    	{
+    		case BasicBull.TypeOfBull.BULL_BABY:
+				Player_Bear.Instance.SetBearAttackBaby(TempBull);
+				BabyBull TempBabyBull = (BabyBull)TempBull;
+    			TempBabyBull.GetHit();
+    			break;
+    	}
+    }
+
+    enum ArrowKeysPressed
+    {
+        KEYS_NONE,
+        KEYS_LEFT,
+        KEYS_RIGHT,
+        KEYS_UP,
+        KEYS_DOWN
+    }
+
+    ArrowKeysPressed NormalControlsUpdate()
+    {
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+            return ArrowKeysPressed.KEYS_LEFT;
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            return ArrowKeysPressed.KEYS_RIGHT;
+        else
+            return ArrowKeysPressed.KEYS_NONE;
+    }
+
+    public void AddBullInsideList(BasicBull EnteringBull)
+    {
+        if (!EnteringBull.IfFacingRight)
+            ListOfBullRight.Add(EnteringBull);
+        else
+            ListOfBullLeft.Add(EnteringBull);
+    }
+
+    public void BullGetPunched(BasicBull TargetBull)
+    {
+		List<BasicBull> TempList = null;
+		if(TargetBull.IfFacingRight)
+           	 TempList = ListOfBullLeft;
+        else
+            TempList = ListOfBullRight;
+
+        TempList.RemoveAt(0);
+
+        Destroy(TargetBull.gameObject);
+    }
+}
---
+++ You Can't Bear The Bulls/Assets/Scripts/Player/Player_Bear.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player_Bear : MonoSingleton<Player_Bear>
+{
+
+    BasicBull TargetedBull = null;
+    BearState CurrentBearState = BearState.BEAR_NONE;
+    public float ConstLerpTime = 0.5f;
+    float LerpTimeLeft = 0;
+
+    public int ConstMaxCharge = 100;
+    int CurrentCharge = 0;
+
+	public float ConstAwayFrom = 2;
+
+    Vector3 MissTowards;
+
+    public Image ChargeBar;
+
+    enum BearState
+    {
+        BEAR_NONE,
+        BEAR_ATTACK_BABY,
+		BEAR_ATTACK_MOTHER,
+		BEAR_ATTACK_FATHER,
+        BEAR_MISS
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+		ChargeBar.fillAmount = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BearUpdate();
+    }
+
+    void BearUpdate()
+    {
+        switch (CurrentBearState)
+        {
+			case BearState. BEAR_ATTACK_BABY:
+                BearAttackBabyUpdate();
+                break;
+			case BearState.BEAR_MISS:
+				BearMissUpdate();
+				break;
+        }
+    }
+
+	void BearAttackBabyUpdate()
+    {
+    	LerpTimeLeft += TimeManager.Instance.GetGameDeltaTime();
+		Vector3 NewTargetPosition;
+    	if(TargetedBull.IfFacingRight)
+			NewTargetPosition = new Vector3(TargetedBull.transform.position.x,transform.position.y,0) + new Vector3(ConstAwayFrom,0,0);
+		else
+			NewTargetPosition = new Vector3(TargetedBull.transform.position.x,transform.position.y,0) + new Vector3(-ConstAwayFrom,0,0);;
+			 
+		transform.position = Vector3.Lerp(new Vector3(transform.position.x,transform.position.y,0), NewTargetPosition, LerpTimeLeft/ConstLerpTime);
+
+		if(transform.position == NewTargetPosition)
+    	{
+    		CurrentBearState = BearState.BEAR_NONE;
+    		GameSceneManager.Instance.BullGetPunched(TargetedBull);
+    		TargetedBull = null;
+    	}
+    }
+
+
+
+    void BearMissUpdate()
+    {
+		LerpTimeLeft += TimeManager.Instance.GetGameDeltaTime();
+		transform.position = Vector3.Lerp(new Vector3(transform.position.x,transform.position.y,0), MissTowards, LerpTimeLeft/ConstLerpTime);
+
+		if(transform.position == MissTowards)
+		{
+			CurrentBearState = BearState.BEAR_NONE;
+		}
+    }
+
+    void IncreaseCharge(int AmountOfNewCharge)
+    {
+		CurrentCharge += AmountOfNewCharge;
+
+    }
+
+    public void SetBearAttackBaby(BasicBull AttackThisBull)
+    {
+    	TargetedBull = AttackThisBull;
+    	CurrentBearState = BearState.BEAR_ATTACK_BABY;
+    	LerpTimeLeft = 0;
+    }
+
+//	public void StopMissing()
+//    {
+//		CurrentBearState = BearState.BEAR_NONE;
+//    }
+
+    public void SetBearMiss(bool IfFacingRight)
+    {
+		CurrentBearState = BearState.BEAR_MISS; 
+		if(IfFacingRight)
+			MissTowards = transform.position + new Vector3(1,0,0);
+		else
+			MissTowards = transform.position + new Vector3(-1,0,0);
+
+		LerpTimeLeft = 0;
+    }
+
+	void OnTriggerEnter2D(Collider2D collision)
+    {
+    	if(collision.tag == "Bull" && (CurrentBearState == BearState.BEAR_ATTACK_MOTHER ))
+    	{
+	        Debug.Log("Bull Enter");   
+	        BasicBull EnteringBull = collision.GetComponent<BasicBull>();
+	        GameSceneManager.Instance.AddBullInsideList(EnteringBull);
+        }
+    }
+}
